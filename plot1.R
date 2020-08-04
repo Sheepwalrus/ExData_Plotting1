aab5d3aa26184181eb5b538a9028ab07d1af53e3
Sheepwalrus <jdphd7@gmail.com>
2020-08-04 14:32:17 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,20 +1,20 @@
 library("data.table")
 
-#Reads in data from file then subsets data for specified dates
+# Reads in data file, only reads in data that has a valid entry
 powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 
-# Prevents histogram from printing in scientific notation
+# Allows Histogram to be display numbers in decimal format
 powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
 # Change Date Column to Date Type
 powerDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
-# Filter Dates for 2007-02-01 and 2007-02-02
+# Cuts the selection of data to values for February 1st. 
 powerDT <- powerDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
 png("plot1.png", width=480, height=480)
 
-## Plot 1
+# First Plot
 hist(powerDT[, Global_active_power], main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 dev.off()
