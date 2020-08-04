3909634dc962bd08a34526d0c7d90510e579871b
Sheepwalrus <jdphd7@gmail.com>
2020-08-04 14:37:40 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ library("data.table")
 # Reads in data file, only reads in data that has a valid entry
 powerDT <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
 
-# Allows Histogram to be display numbers in decimal format
+# Forces decimal format
 powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
 
 # Making a POSIXct date capable of being filtered and graphed by time of day
@@ -12,6 +12,7 @@ powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"
 # Cuts the selection of data to values for February 1st and 2nd
 powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
 
+# Creates a blank plot
 png("plot2.png", width=480, height=480)
 
 # Second Plot
